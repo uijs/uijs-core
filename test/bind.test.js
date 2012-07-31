16292cd5f38a021e8cebbd0f42da5b17b9118975
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-07-31 16:14:07 +0300

    Add watch/unwatch

--- test/bind.test.js
+++ test/bind.test.js
@@ -1,7 +1,12 @@
 var assert = require('assert');
 var bind = require('..').bind;
+var EventEmitter = require('..').events.EventEmitter;
+
 var autobind = bind.autobind;
 
+// play around with some usage options
+
+// create a 'bind promise' and assign it in the object literal later.
 var foobind = bind(obj, 'foo', function() { return 'hello' });
 
 var obj = autobind({
@@ -11,17 +16,28 @@ var obj = autobind({
   goo: function() { return 'goo'; },
 });
 
+assert(obj.foo, 'hello');
+
+// this will not work because `xoo` is not bound.
 obj.xoo = foobind;
+assert(obj.xoo.$bind);
 
-var goobind = bind(obj, 'goo', function() { return 88 });
 
-bind(obj, 'foo', function() { return 77; });
-obj.foo = goobind;
+// set up a watch on `foo`
 
+var foo_changed = -1;
+obj.watch('foo', function(new_foo, bound) {
+  foo_changed = new_foo;
+});
+
+// assign the result of bind back to `foo`. should work
+obj.foo = bind(obj, 'foo', function() { return 88 });
 assert(obj.foo === 88);
+assert(foo_changed === 88);
 
 // bench(obj, 'x');
 
+// bind without assignment. should work
 bind(obj, 'hello', function() { return 'world'; });
 
 // bench(obj, 'x');
@@ -31,10 +47,24 @@ assert(obj.x === 5);
 assert(obj.y === 10);
 assert(obj.hello === 'world');
 
+// reassign a value to a bound property - should convert to a value
 obj.hello = 99;
+assert(obj.hello === 99);
 
 // bench(obj, 'hello');
 // bench(obj, 'x');
+
+// watch a regular value (behind the scenes it turns to a bound value)
+var koo_changed = -1;
+obj.koo = 8989;
+obj.watch('koo', function(newval) { koo_changed = newval; });
+obj.koo = 777;
+assert(obj.koo === 777);
+assert(koo_changed === 777);
+obj.koo = 444;
+assert(koo_changed === 444);
+bind(obj, 'koo', function() { return 'yeah!' });
+assert(koo_changed === 'yeah!');
 
 // ----
 
